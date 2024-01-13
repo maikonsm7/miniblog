e00c81ee5902eb040957ac21405e20bddb147153
Maikon <maikonsm7@gmail.com>
2024-01-13 12:48:30 -0400

    create context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext()
+
+export function AuthContextProvider({children, value}){
+    return(
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
