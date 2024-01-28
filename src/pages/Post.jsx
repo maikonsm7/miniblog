0fbf468c6981b487be35b666a57b9bfeff8760df
maikonmonteiro <134222239+maikonsm7@users.noreply.github.com>
2024-01-28 12:50:56 -0400

    create post

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,14 +1,30 @@
 // pegar parametro da URL designado no App.js nas rotas
 import { useParams } from "react-router-dom"
 import style from './Post.module.css'
+import { useFetchDocument } from "../hooks/useFetchDocument"
 
 const Post = () => {
     const {id} = useParams()
+    const {document: post, loading} = useFetchDocument('posts', id)
 
     return(
-        <div>
-            <h3>Post</h3>
-            <p>{id}</p>
+        <div className={style.post_container}>
+            {loading && (
+                <p>carregando...</p>
+            )}
+            {post && (
+                <>
+                <h3>{post.title}</h3>
+                <img src={post.image} alt={post.title} />
+                <p>{post.body}</p>
+                <h4>Este post trata sobre:</h4>
+                <div className={style.tags}>
+                {post.tagsArray.map(tag => (
+                    <p key={tag}><span>#</span>{tag}</p>
+                    ))}
+            </div>
+                </>
+            )}
         </div>
     )
 }
