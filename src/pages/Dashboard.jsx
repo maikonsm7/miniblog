692b57b27d18da723fe6ded0b2adcc50eb3ed9ee
maikonmonteiro <134222239+maikonsm7@users.noreply.github.com>
2024-01-28 20:08:04 -0400

    initial update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,9 @@
 import styles from './Dashboard.module.css'
+import { Link } from 'react-router-dom'
+import {useAuthValue} from '../context/AuthContext'
+import {useFetchDocuments} from '../hooks/useFetchDocuments'
 const Dashboard = () => {
+    const {user} = useAuthValue()
     return(
         <div>
             <h3>Dashboard</h3>
