e0fddd1534e1e964b2b4d054017c0cdeb93ea129
Maikon <maikon@192.168.100.6>
2024-02-04 15:26:02 -0400

    update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,9 +4,24 @@ import {useAuthValue} from '../context/AuthContext'
 import {useFetchDocuments} from '../hooks/useFetchDocuments'
 const Dashboard = () => {
     const {user} = useAuthValue()
+    const uid = user.uid
+    const {documents: posts, loading} = useFetchDocuments('posts', null, uid)
     return(
         <div>
             <h3>Dashboard</h3>
+            <p>Gerencie seus posts</p>
+            {posts && posts.length === 0 ? (
+                <dir className={styles.noposts}>
+                    <p>Nao foram encontrados posts</p>
+                    <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
+                </dir>
+            ):(
+                <div>
+                    <p>Tem posts!</p>
+                </div>
+            )}
+            {loading && <p>Carregando...</p>}
+            {posts && posts.map(post => <h4>{post.title}</h4>)}
         </div>
     )
 }
